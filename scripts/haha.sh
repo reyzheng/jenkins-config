aa13a89760c96a0051f29db3683b3a43273dc61c
reyzheng <reyzheng@gmail.com>
2023-02-27 10:39:09 +0800

    Update haha.sh

--- scripts/haha.sh
+++ scripts/haha.sh
@@ -1,9 +1,1 @@
-if [ -z ${BUILD_BRANCH+x} ]; then 
-    echo "none"
-    var="PARAM_COV_PROJECT"
-    echo "${!var}"
-else
-    echo "set"
-    var=$BUILD_BRANCH"_PARAM_COV_PROJECT"
-    echo "${!var}"
-fi
+echo "post stage executed"
